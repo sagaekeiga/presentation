af9d8ecbaba929b70a6e0a8a4a064edb49950b08
sagae keiga <sagae5.28rujeae@gmail.com>
2017-05-01 03:42:02 +0000

    finish

--- Gemfile
+++ Gemfile
@@ -44,6 +44,9 @@ gem 'gon'
 gem 'redis-objects'
 gem 'public_activity'
 
+gem 'qiita-markdown'
+gem 'github-linguist'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
   def gravatar_for(user, size = 80, secure = true)
     Gravatar.new(user.email).image_url(size: size, secure: secure)
   end
+  
+  def qiita_markdown(markdown)
+    processor = Qiita::Markdown::Processor.new(hostname: "example.com")
+    processor.call(markdown)[:output].to_s.html_safe
+  end
 end
